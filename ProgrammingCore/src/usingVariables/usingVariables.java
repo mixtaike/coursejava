c7e3d4cf061364406ec2e2bfe8bd1e03a111c96c
Mixtaike <mixtaike@gmail.com>
2022-05-30 17:07:58 -0300

    Using Variables

---
+++ ProgrammingCore/src/usingVariables/usingVariables.java
@@ -0,0 +1,44 @@
+package usingVariables;
+
+public class usingVariables {
+	
+	public static void main(String[] args) {
+		
+		/*Declando um variavel e atribuindo valor*/
+		
+		int myNumber;
+		
+		myNumber = 88;
+		
+		System.out.println(myNumber);
+		
+		/*OU*/
+		
+		int myNumber2 = 88;
+		
+		
+		/*TIPOS DE DADOS - PRIMITIVOS*/
+		
+		/*NUMEROS INTEIROS
+		 byte - 8 BITS - Valor mínimo é -128 e o valor máximo é 127.
+		 short - 16 - BITS - entre -32.768 até 32.767 
+		 int - 32 BITS - entre -2.147.483.64 até 2.147.483.647
+		 long - 64 BITS - -9.223.372.036.854.775.808 (-2?³) até 9.223.372.036.854.775.807 (2?³?¹)
+		 bigDecimal para valores monetarios
+		  */
+		
+		/*NUMEROS COM PONTO FLUTUANTE
+		float - 32 bits de precisão única -  Sua faixa de valor é ilimitada. Exemplo: float f1 = 234.5f
+		double - 64 bits de precisão dupla. Seu valor é ilimitado. - Exemplo: double d1 = 12.3
+		
+		 * */
+		
+		/*CARCTERES
+		char  - caractere unicode de 16 bits. Sua faixa de valor está entre '\u0000' (ou 0) até '\uffff' (ou 65.535 inclusive).
+		  */
+		
+	}
+	
+	
+
+}
