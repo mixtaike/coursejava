1cc1202c258cd33cd8978feaafa012a6943cac0d
Mixtaike <mixtaike@gmail.com>
2022-05-30 17:30:59 -0300

    While Loops

---
+++ ProgrammingCore/src/whileLoops/Application.java
@@ -0,0 +1,28 @@
+package whileLoops;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		/* control+shift+f organizar código */
+		
+		/*Estruturas lógica simples*/
+		
+		int value = 10;
+		boolean loop = value < 20;
+		boolean loop2 = 6 < 1;
+		System.out.println(loop);
+		System.out.println(loop2);
+		int value2 = 0;
+		
+		/*Loop*/
+		
+		while(value2  < 10)
+		{
+			System.out.println("Hello" + value2);
+			value2 = value2 + 1;
+		}
+		
+	}
+
+}
