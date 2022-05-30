454a7f60171ac76b5f305bec54a39efeff251499
Mixtaike <mixtaike@gmail.com>
2022-05-30 17:20:36 -0300

    Strings: Working Wiht Text

---
+++ ProgrammingCore/src/stringsWorkingWithText/Application.java
@@ -0,0 +1,36 @@
+package stringsWorkingWithText;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		
+		int myInt = 7;
+		
+		//String é uma classe  - um tipo de objeto que pode armazenar texto
+		String text = "Hello";
+		
+		String blank = " ";
+		
+		String name = "Julia";
+		
+		String greeting = text + blank + name;
+		
+		System.out.println(greeting);
+		System.out.println(text);
+		
+		
+		/*concatenação*/
+		
+		System.out.println("My integer is "+myInt);
+		
+		
+		
+		double myDouble = 7.8;
+		
+		System.out.println("My number is: "+ myDouble);
+		
+		
+		
+	}
+
+}
